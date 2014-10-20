128cfdc85c38f7025811e3762236111fb4bb92a1
saddamhossain <engg.rana.cse.seu@gmail.com>
2014-10-20 13:43:20 +0600

    Testing Update

--- UniversityFinal/DLL/DAO/Course.cs
+++ UniversityFinal/DLL/DAO/Course.cs
@@ -11,9 +11,6 @@ namespace UniversityFinal.DLL.DAO
         public int CourseID { get; set; }
         public string CourseTitle { get; set; }
         public string CourseName { get; set; }
-       // public double Score { get; set; }
-       // public double AvgScore { get; set; }
-       // public string  GradeLetter  { get; set; }
         public string EnrollmentDate { get; set; }
         public int StudentID { get; set; }
 
